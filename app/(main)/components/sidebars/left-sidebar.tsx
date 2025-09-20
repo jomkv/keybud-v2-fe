6975f3c685e7176ef6281073af1ac384661a2866
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-09-20 18:02:37 +0800

    feat: nav responsive styles for icon and font weight

--- app/(main)/components/sidebars/left-sidebar.tsx
+++ app/(main)/components/sidebars/left-sidebar.tsx
@@ -1,53 +1,78 @@
+"use client";
+
 import { Icon } from "@iconify/react";
 import Link from "next/link";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import CreatePostModal from "@/app/(main)/components/modals/create-post-modal";
+import { Page } from "@/@types/navigation";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { setPage } from "@/store/slices/navigationSlice";
+import { RootState } from "@/store/store";
 
 const LEFT_NAV_ITEMS: {
   icon: string;
   url: string;
   title: string;
+  value: Page | null;
 }[] = [
   {
     title: "Home",
     url: "/",
     icon: "ph:house",
+    value: "home",
   },
   {
     title: "Messages",
     url: "/messages",
     icon: "ph:envelope-simple",
+    value: "messages",
   },
   {
     title: "Notifications",
     url: "/notifications",
     icon: "ph:bell-simple",
+    value: "notifications",
   },
   {
     title: "Search",
     url: "/search",
     icon: "ph:magnifying-glass",
+    value: "search",
   },
   {
     title: "Profile",
     url: "/johndoe",
     icon: "ph:user",
+    value: "profile",
   },
   {
     title: "Settings",
     url: "/settings",
     icon: "ph:gear",
+    value: "settings",
   },
   {
     title: "Logout",
     url: "#",
     icon: "ph:arrow-line-right",
+    value: null,
   },
 ];
 
 export default function LeftSidebar() {
+  const dispatch = useAppDispatch();
+  const { currentPage } = useAppSelector(
+    (state: RootState) => state.navigation
+  );
+
+  const handleNavClick = (value: Page | null) => {
+    if (value === null) return;
+
+    dispatch(setPage(value));
+  };
+
   return (
     <div className="flex flex-col justify-between h-full w-full">
       <div className="flex flex-col gap-3 items-end 2xl:items-start">
@@ -62,11 +87,22 @@ export default function LeftSidebar() {
         </Link>
         {LEFT_NAV_ITEMS.map((navItem, key) => (
           <Link
-            className="flex items-center p-3 2xl:p-5 2xl:ps-3 justify-start gap-5 h-14 w-auto hover:bg-neutral-500 rounded-full"
+            className={`flex items-center p-3 2xl:p-5 2xl:ps-3 justify-start gap-5 h-14 w-auto hover:bg-neutral-500 rounded-full ${
+              currentPage === navItem.value ? "font-bold" : ""
+            }`}
             href={navItem.url}
             key={key}
+            onClick={() => handleNavClick(navItem.value)}
           >
-            <Icon className="text-3xl" icon={navItem.icon} />
+            <Icon
+              key={key}
+              className="text-3xl"
+              icon={
+                currentPage === navItem.value
+                  ? `${navItem.icon}-fill`
+                  : navItem.icon
+              }
+            />
             <span className="hidden 2xl:block text-xl">{navItem.title}</span>
           </Link>
         ))}
