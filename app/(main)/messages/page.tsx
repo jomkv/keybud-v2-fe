76ed623b8e6f3e90812cc7770cbbae513c3df8b2
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-09-23 22:56:07 +0800

    feat: messages page init, still needs hella work

--- app/(main)/messages/page.tsx
+++ app/(main)/messages/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search, Settings } from "lucide-react";
 import React from "react";
 
 function Messages() {
-  return <div>hello messages</div>;
+  return (
+    <>
+      <div className="flex justify-between items-end p-3">
+        <p className="font-semibold text-3xl">Messages</p>
+        <Button size="icon" variant="ghost">
+          <Settings style={{ width: "90%", height: "90%" }} />
+        </Button>
+      </div>
+      {/* Search Input with Icon */}
+      <div className="relative p-3">
+        <div className="absolute inset-y-0 left-6 flex items-center pointer-events-none">
+          <Search className="h-5 w-5 text-gray-500" />
+        </div>
+        <Input
+          placeholder="Search Messages"
+          className="pl-12 border-neutral-700 rounded-full text-white"
+        />
+      </div>
+    </>
+  );
 }
 
 export default Messages;
