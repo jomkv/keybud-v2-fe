a020118ca0183a0a3d1a4da6c1dca76a7fdbdce2
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-09-27 14:10:02 +0800

    feat: messages settings

--- app/(main)/messages/page.tsx
+++ app/(main)/messages/page.tsx
@@ -1,18 +1,41 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Search, Settings } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import ConversationCard from "@/app/(main)/messages/components/cards/conversation-card";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 function Messages() {
+  const [enableNotifs, setEnableNotifs] = useState<boolean>(true);
+
   return (
     <>
       {/* Header */}
       <div className="flex justify-between items-end p-3">
         <p className="font-semibold text-3xl">Messages</p>
-        <Button size="icon" variant="ghost">
-          <Settings style={{ width: "90%", height: "90%" }} />
-        </Button>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button size="icon" variant="ghost">
+              <Settings style={{ width: "90%", height: "90%" }} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuCheckboxItem
+              checked={enableNotifs}
+              onCheckedChange={setEnableNotifs}
+            >
+              Receive Email Notifications
+            </DropdownMenuCheckboxItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
 
       <div className="flex flex-col gap-2">
