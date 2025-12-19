5544c006028b539a29170b4356c626777ff98925
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-12-19 22:30:40 +0800

    feat: apply client-side verification for session at main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,27 @@
+"use client";
+
 import LeftSidebar from "@/app/(main)/components/sidebars/left-sidebar";
 import RightSidebar from "@/app/(main)/components/sidebars/right-sidebar";
+import { useUser } from "@/hooks/use-user";
+import { redirect } from "next/navigation";
 
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = useUser();
+
+  // If the user is undefined (still hydrating from Redux), show spinner
+  if (user === undefined) {
+    return <div className="h-screen bg-black" />;
+  }
+
+  // If no user exists after hydration, redirect to login
+  if (!user) {
+    redirect("/login");
+  }
+
   return (
     <div className="flex justify-center min-h-screen relative">
       <div className="min-w-[5rem] max-w-[20rem] w-[calc(25vw-10rem)] sticky top-0 h-screen py-5 flex-shrink-0 overflow-y-auto">
