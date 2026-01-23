3cb4a61cdd9c7f87806bedc3205b31efe6aed66f
Jom Karlo Verzosa <jomkarlov@gmail.com>
2026-01-23 22:07:27 +0800

    feat: integrate types from contracts module to scokets

--- app/socket.tsx
+++ app/socket.tsx
@@ -1,8 +1,25 @@
 "use client";
 
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
+import {
+  AuthClientToServerEvents,
+  AuthServerToClientEvents,
+  MessageClientToServerEvents,
+  MessageServerToClientEvents,
+} from "@jomkv/keybud-v2-contracts";
 
-export const authSocket = io(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth`, {
+export const authSocket: Socket<
+  AuthServerToClientEvents,
+  AuthClientToServerEvents
+> = io(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth`, {
+  autoConnect: false,
+  transports: ["websocket"],
+});
+
+export const messageSocket: Socket<
+  MessageServerToClientEvents,
+  MessageClientToServerEvents
+> = io(`${process.env.NEXT_PUBLIC_BACKEND_URL}/message`, {
   autoConnect: false,
   transports: ["websocket"],
 });
